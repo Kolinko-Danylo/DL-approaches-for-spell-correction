98a86ba32e0549707a901859367e426db92fc3a3
Kolinko-Danylo <kolinko@ucu.edu.ua>
2020-04-01 14:23:07 +0000

    seq2seq dataloader

--- dataload/dataloader.py
+++ dataload/dataloader.py
@@ -10,9 +10,11 @@ from gensim.models.wrappers import FastText
 from torch.utils import data
 import string
 
+from bpemb import BPEmb
 
 
-class DataLoader(data.Dataset):
+
+class SemicharDataset(data.Dataset):
     def __init__(self, root_path, seq_length):
         """Initialize the dataset"""
         self.chars = None
@@ -107,37 +109,170 @@ class DataLoader(data.Dataset):
         return torch.from_numpy(X), torch.from_numpy(y)
 
 
+
+
+
+
+
+
+class seq2seqDataset(data.Dataset):
+    def __init__(self, root_path, seq_length, embed_dim, embed_vec_space):
+        """Initialize the dataset"""
+
+        self.root_path = root_path
+        self.seq_length = seq_length
+        self.tokenized_data = self.tokenize(root_path)
+        self.embed = BPEmb(lang="en", vs=embed_vec_space, add_pad_emb=True)
+
+
+
+    def tokenize(self, root_path):
+  
+        with open(root_path, 'r') as f:
+            text = f.read()
+        lt = len(text)
+        print(lt)
+
+        splitted = []
+        start = 0
+
+        while True:
+
+          flag = (1 if self.seq_length >= lt - start else 0)
+          cur_text = text[start: start + min(self.seq_length, lt - start)]
+          last_chunk_len = (0 if cur_text[-1].isspace() else len(cur_text.split()[-1]))
+          
+          start += self.seq_length - last_chunk_len
+
+          if last_chunk_len == len(cur_text.strip()):
+            start += self.seq_length
+            continue
+          splitted.append(cur_text[:-last_chunk_len].strip())
+
+          if flag:
+            break
+
+        return splitted
+
+    def augment(self, seq):
+        augmentator = nac.KeyboardAug(aug_char_min=0, aug_char_p=0.4, aug_word_p=0.5, aug_word_min=0,
+                                 aug_word_max=len(seq) // 5, special_char=False)
+
+        augmented_data = augmentator.augment(seq)
+        return seq
+
+    def get_encodes(self, seq, use_aug=False):
+        if use_aug:
+            seq = self.augment(seq)
+        
+
+        def padded_encode(x):
+            res = np.full((self.seq_length, self.embed.dim), self.embed['<pad>'])
+            enc = self.embed.encode_ids(x)
+            
+            res[:len(enc)] = self.embed.vectors[enc]
+            length = len(enc)
+            
+            res = np.insert(res, 0, np.full((self.embed.dim), -1), 0)
+            length += 1
+
+            if use_aug:
+              res = np.insert(res, length, np.full((self.embed.dim), -2), 0)
+              length += 1
+
+            return np.array([length]), res
+
+        len_vec, res_arr = padded_encode(seq)  
+
+                  
+        
+        # len_vec, perm_idx = torch.from_numpy(len_vec).sort(0, descending=True)
+        # res_arr = res_arr[perm_idx]
+
+        return len_vec, res_arr
+
+    def __len__(self):
+        return len(self.tokenized_data)
+
+    def __getitem__(self, index):
+        sequence = self.tokenized_data[index]
+        
+        lengths_x, X = self.get_encodes(sequence, use_aug=True)
+        lengths_y, Y = self.get_encodes(sequence)
+        
+        return tuple([torch.from_numpy(arr) for arr in (lengths_x, X, lengths_y, Y)])
+
+
+
+# def get_encodes(arr, seq_length, use_aug=False):
+#   if use_aug:
+#     aug_rr = nac.KeyboardAug(aug_char_min=0, aug_char_max=None, aug_char_p=0.4, aug_word_p=0.4, aug_word_min=0, aug_word_max=arr.size//3, special_char=False)
+    
+#     augmented_data = list(map(lambda x: aug_rr.augment(x), arr.ravel()))
+#     arr = np.array(augmented_data).reshape(arr.shape)
+#   flat_arr = arr.ravel()
+
+#   def padded_encode(x):
+#     k = np.full((seq_length,), bpemb_en.vs)
+#     enc = np.array(bpemb_en.encode_ids(x))
+#     k[:enc.size] = enc
+
+
+#     return enc.size, k
+
+#   res_arr = np.empty((*flat_arr.shape, seq_length), dtype="int32")
+#   len_vec = np.empty( (arr.shape[0]))
+#   for i in range(flat_arr.size):
+
+#     res = padded_encode(flat_arr[i])  
+#     res_arr[i] = res[1]
+#     len_vec[i] = res[0]
+
+#   if not use_aug:
+#     res_arr = np.insert(res_arr, 0, 1, 1)
+#   len_vec, perm_idx = torch.from_numpy(len_vec).sort(0, descending=True)
+#   res_arr = res_arr[perm_idx]
+
+#   leng, res = len_vec, one_hot_encode(res_arr, bpemb_en.vs + 1)
+
+#   leng += (1 if not use_aug else 0)
+#   leng, res = leng, torch.from_numpy(res)
+
+#   return leng, res
+
 # def get_batches(arr, batch_size, seq_length):
 #     '''Create a generator that returns batches of size
 #        batch_size x seq_length from arr.
-#
+       
 #        Arguments
 #        ---------
 #        arr: Array you want to make batches from
 #        batch_size: Batch size, the number of sequences per batch
 #        seq_length: Number of encoded chars in a sequence
 #     '''
-#
-#     batch_size_total = batch_size * seq_length
+    
 #     # total number of batches we can make
-#     n_batches = len(arr) // batch_size_total
-#
+#     n_batches = len(arr)//batch_size
+    
 #     # Keep only enough characters to make full batches
-#     arr = arr[:n_batches * batch_size_total]
-#
+#     arr = arr[:n_batches * batch_size]    
+
+  
+
 #     # Reshape into batch_size rows
 #     arr = arr.reshape((batch_size, -1))
 #     # iterate through the array, one sequence at a time
-#     for n in range(0, arr.shape[1], seq_length):
+#     for n in range(0, arr.shape[1]):
 #         # The features
-#         base = arr[:, n:n + seq_length]
-#         y = np.vectorize(emb.get_int)(base)
-#
+#         base = arr[:, n:n+1]
+#         # y = np.vectorize(get_int)(base)
+
 #         x = base.copy()
-#         x = emb.get_encodes(x, use_aug=True)
-#
-#         yield x, y
+#         # y = one_hot_encode(y, len(words))
+#         lengths_x, x = get_encodes(x, seq_length, use_aug=True)
+#         lengths_y, y = get_encodes(base, seq_length)
+        
+#         yield lengths_x, x, lengths_y, y
 
-
-
-
+if __name__ == '__main__':
+    ds = seq2seqDataset("data/big.txt", 40, 1000, 50)
---
+++ model/seq2seq.py
@@ -0,0 +1,123 @@
+import random
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+import numpy as np
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, n_layers=1, dropout=0.1):
+        super(EncoderRNN, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+        self.gru = nn.GRU(input_size, hidden_size, n_layers, dropout=self.dropout, bidirectional=True, batch_first=True)
+        
+    def forward(self, input_seqs, input_lengths, hidden=None):
+        # Note: we run this all at once (over multiple batches of multiple sequences)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(input_seqs, input_lengths, batch_first=True)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, output_lengths = torch.nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True) # unpack (back to padded)
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:] # Sum bidirectional outputs
+        return outputs, hidden
+
+
+
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        
+        self.method = method
+        self.hidden_size = hidden_size
+        
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(1, hidden_size))
+
+    def forward(self, hidden, encoder_outputs):
+        max_len = encoder_outputs.size(1)
+        this_batch_size = encoder_outputs.size(0)
+
+        # Create variable to store attention energies
+        attn_energies = torch.zeros(this_batch_size, max_len) # B x S
+
+
+        attn_energies = attn_energies.cuda()
+
+        # For each batch of encoder outputs
+        for b in range(this_batch_size):
+            # Calculate energy for each encoder output
+            for i in range(max_len):
+                attn_energies[b, i] = self.score(hidden[b, :].squeeze(0), encoder_outputs[b, i])
+
+        # Normalize energies to weights in range 0 to 1, resize to 1 x B x S
+        return F.softmax(attn_energies).unsqueeze(1)
+    
+    def score(self, hidden, encoder_output):
+        
+        if self.method == 'dot':
+            energy = hidden.dot(encoder_output)
+            return energy
+        
+        elif self.method == 'general':
+            energy = self.attn(encoder_output)
+            energy = hidden.dot(energy)
+            return energy
+        
+        elif self.method == 'concat':
+            energy = self.attn(torch.cat((hidden, encoder_output), 1))
+            energy = self.v.dot(energy)
+            return energy
+
+
+class LuongAttnDecoderRNN(nn.Module):
+    def __init__(self, attn_model, input_size, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(LuongAttnDecoderRNN, self).__init__()
+
+        # Keep for reference
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # Define layers
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(input_size, hidden_size, n_layers, dropout=dropout, batch_first=True)
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        
+        # Choose attention model
+        if attn_model != 'none':
+            self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_seq, last_hidden, encoder_outputs):
+
+        rnn_output, hidden = self.gru(input_seq, last_hidden)
+
+        # Calculate attention from current RNN state and all encoder outputs;
+        # apply to encoder outputs to get weighted average
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs) # B x S=1 x N
+
+        # Attentional vector using the RNN hidden state and context vector
+        # concatenated together (Luong eq. 5)
+        rnn_output = rnn_output.squeeze(1) # S=1 x B x N -> B x N
+        context = context.squeeze(1)       # B x S=1 x N -> B x N
+
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = F.tanh(self.concat(concat_input))
+
+        # Finally predict next token (Luong eq. 6, without softmax)
+        output = F.log_softmax(self.out(concat_output))
+
+        # Return final output, hidden state, and attention weights (for visualization)
+        return output, hidden, attn_weights
